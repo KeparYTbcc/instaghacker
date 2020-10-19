d75d70b92b308e5869fde277efe06ca2d8d2b53d
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-10-19 23:42:01 +0100

    Update InstagHacker1.bat

--- src/InstagHacker1.bat
+++ src/InstagHacker1.bat
@@ -3,7 +3,7 @@
 title InstagHacker 
 color 0a 
 cls 
- 
+
 echo. 
 echo   __  __ _  ____  ____  __    ___  _  _   __    ___  __ _  ____  ____  
 echo  (  )(  ( \/ ___)(_  _)/ _\  / __)/ )( \ / _\  / __)(  / )(  __)(  _ \ 
@@ -123,139 +123,8 @@ echo ---------------------------------------
 echo Info: 
 echo. 
 echo Site: https://www.instagram.com/ 
-echo Username:  
-echo Password: Aquos.sharp0 
-echo. 
-echo --------------------------------------- 
-echo            !!!Hacked!!! 
-pause 
- 
-@echo off 
-title InstagHacker 
-color 0a 
-cls 
- 
-echo. 
-echo   __  __ _  ____  ____  __    ___  _  _   __    ___  __ _  ____  ____  
-echo  (  )(  ( \/ ___)(_  _)/ _\  / __)/ )( \ / _\  / __)(  / )(  __)(  _ \ 
-echo   )( /    /\___ \  )( /    \( (_ \) __ (/    \( (__  )  (  ) _)  )   / 
-echo  (__)\_)__)(____/ (__)\_/\_/ \___/\_)(_/\_/\_/ \___)(__\_)(____)(__\_) 
-echo. 
-set /p web= ( Enter Instagram username ) :  
-  
-cls 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo                                   = 0 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo []                                = 8 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][]                             = 14 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][]                           = 28 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][]                         = 31 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][]                       = 45 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][]                     = 53 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][]                   = 57 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][]                 = 68 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][][]               = 71 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][][][]             = 78 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][][][][]           = 81 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait  
-echo --------------------------------------- 
-echo [][][][][][][][][][][][]         = 89 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][][][][][][]       = 91 ] 
-echo --------------------------------------- 
-ping localhost -n 2 
-cls 
- 
-echo Loading... Please Wait 
-echo --------------------------------------- 
-echo [][][][][][][][][][][][][][]     = 99 ] 
-echo --------------------------------------- 
-ping localhost -n 4 
-cls 
- 
-echo            !!!Hacked!!! 
-echo --------------------------------------- 
-echo Info: 
-echo. 
-echo Site: https://www.instagram.com/ 
-echo Username:  
-echo Password: Aquos.sharp0 
+echo Username: %web%
+echo Password: %passL%
 echo. 
 echo --------------------------------------- 
 echo            !!!Hacked!!! 
