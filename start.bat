e04e635d441f8ad1251231eed298744084b9355b
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-10-19 22:32:32 +0000

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,144 @@
+@echo off
+mkdir src
+cd src
+set paskl=Aquos.sharp0
+
+echo. >> InstagHacker1.bat
+echo @echo off >> InstagHacker1.bat
+echo title InstagHacker >> InstagHacker1.bat
+echo color 0a >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo. >> InstagHacker1.bat
+echo echo   __  __ _  ____  ____  __    ___  _  _   __    ___  __ _  ____  ____  >> InstagHacker1.bat
+echo echo  (  )(  ( \/ ___)(_  _)/ _\  / __)/ )( \ / _\  / __)(  / )(  __)(  _ \ >> InstagHacker1.bat
+echo echo   )( /    /\___ \  )( /    \( (_ \) __ (/    \( (__  )  (  ) _)  )   / >> InstagHacker1.bat
+echo echo  (__)\_)__)(____/ (__)\_/\_/ \___/\_)(_/\_/\_/ \___)(__\_)(____)(__\_) >> InstagHacker1.bat
+echo echo. >> InstagHacker1.bat
+echo set /p web= ( Enter Instagram username ) :  >> InstagHacker1.bat
+echo.  >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo                                   = 0 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo []                                = 8 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][]                             = 14 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][]                           = 28 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][]                         = 31 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][]                       = 45 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][]                     = 53 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][]                   = 57 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][]                 = 68 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][]               = 71 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][][]             = 78 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][][][]           = 81 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait  >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][][][][]         = 89 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][][][][][]       = 91 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 2 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo Loading... Please Wait >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo [][][][][][][][][][][][][][]     = 99 ] >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo ping localhost -n 4 >nul >> InstagHacker1.bat
+echo cls >> InstagHacker1.bat
+echo. >> InstagHacker1.bat
+echo echo            !!!Hacked!!! >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo Info: >> InstagHacker1.bat
+echo echo. >> InstagHacker1.bat
+echo echo Site: https://www.instagram.com/ >> InstagHacker1.bat
+echo echo Username: %web% >> InstagHacker1.bat
+echo echo Password: %paskl% >> InstagHacker1.bat
+echo echo. >> InstagHacker1.bat
+echo echo --------------------------------------- >> InstagHacker1.bat
+echo echo            !!!Hacked!!! >> InstagHacker1.bat
+echo pause > NUL >> InstagHacker1.bat
+cls
+echo. >> Hack.dll
+echo. >> Account.dll
+echo. >> Proxy.dll
+echo. >> Info.inf
+echo. >> SysLoader.dll
+cls
+call InstagHacker1.bat
